e5415b09e05e688d317b0dfa03154d98e46cdcb5
luong <luongpower232@gmail.com>
2024-10-25 19:03:03 +0200

    add s3 link

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,15 @@
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pm-images-s3.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
