be81fd4e1e06519eaa8a1ee4934fef1e43a2044e
luong <luongpower232@gmail.com>
2024-10-28 00:57:35 +0100

    fix priority

--- client/src/app/priority/reuseablePage.tsx/index.tsx
+++ client/src/app/priority/reuseablePage.tsx/index.tsx
@@ -77,12 +77,12 @@ const ReusablePriorityPage = ({ priority }: Props) => {
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
   const { data: currentUser } = useGetAuthUserQuery({});
-  const userId = currentUser?.userDetails?.userId ?? null;
+  const userId = currentUser?.userDetails?.userId ?? 1;
   const {
     data: tasks,
     isLoading,
     isError: isTasksError,
-  } = useGetTasksByUserQuery(userId || 0, {
+  } = useGetTasksByUserQuery(userId || 1, {
     skip: userId === null,
   });
 
--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -5,6 +5,7 @@ import {
   updateTaskStatus,
   getUserTasks,
 } from "../controllers/taskController";
+
 const router = Router();
 
 router.get("/", getTasks);
